21d9943c606bbe1935686c59e74ae9258c126e76
Naiara Martins <naiaraxf@gmail.com>
2023-12-08 18:02:59 -0300

    Task 1 completed :white_check_mark:

--- src/database/db.py
+++ src/database/db.py
@@ -1,2 +1,6 @@
-# conexão com o banco de dados
-# alteração para commit
+from pymongo import MongoClient
+from os import environ
+
+client = MongoClient(environ.get("MONGO_URI", "mongodb://localhost:27017"))
+
+db = client[environ.get("DB_NAME", "test_db_traduzo")]
