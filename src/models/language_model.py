9d76d59b5b7ab218249d2307933fa932a26279a7
Naiara Martins <naiaraxf@gmail.com>
2023-12-08 19:16:10 -0300

    Task 4 completed :white_check_mark:

--- src/models/language_model.py
+++ src/models/language_model.py
@@ -8,6 +8,16 @@ class LanguageModel(AbstractModel):
     def __init__(self, json_data):
         super().__init__(json_data)
 
+    @classmethod
+    def list_dicts(cls):
+        data = cls._collection.find()
+        languages = []
+        for language in data:
+            languages.append(
+                {"name": language["name"], "acronym": language["acronym"]}
+            )
+        return languages
+
     def to_dict(self):
         return {
             "name": str(self.data["name"]),
