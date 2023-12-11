979a5cdbf83a6ef83d4b542a1a7b555430aa94e2
Naiara Martins <naiaraxf@gmail.com>
2023-12-11 17:29:17 -0300

    Task 6 completed :white_check_mark:

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from models.language_model import LanguageModel
+from deep_translator import GoogleTranslator
 
 translate_controller = Blueprint("translate_controller", __name__)
 
@@ -7,11 +8,29 @@ translate_controller = Blueprint("translate_controller", __name__)
 @translate_controller.route("/", methods=["GET", "POST"])
 def index():
     languages = LanguageModel.list_dicts()
+    if request.method == "POST":
+        text_to_translate = request.form.get("text-to-translate")
+        translate_from = request.form.get("translate-from")
+        translate_to = request.form.get("translate-to")
+    else:
+        text_to_translate = "O que deseja traduzir?"
+        translate_from = "pt"
+        translate_to = "en"
+
+    translated = translated_text(
+        text_to_translate, translate_from, translate_to
+    )
     return render_template(
         "index.html",
         languages=languages,
-        text_to_translate="O que deseja traduzir?",
-        translate_from="pt",
-        translate_to="en",
-        translated="What do you want to translate?",
+        text_to_translate=text_to_translate,
+        translate_from=translate_from,
+        translate_to=translate_to,
+        translated=translated,
     )
+
+
+def translated_text(text_to_translate, translate_from, translate_to):
+    return GoogleTranslator(
+        source=translate_from, target=translate_to
+    ).translate(text_to_translate)
