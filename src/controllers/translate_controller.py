ba7f79ec09d180a6e61dcb71ecc6bad7add9c290
Naiara Martins <naiaraxf@gmail.com>
2023-12-11 18:06:20 -0300

    Task 7 completed :white_check_mark:

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -30,6 +30,25 @@ def index():
     )
 
 
+@translate_controller.route("/reverse", methods=["POST"])
+def index_reverse():
+    languages = LanguageModel.list_dicts()
+    text_to_translate = request.form.get("text-to-translate")
+    translate_from = request.form.get("translate-from")
+    translate_to = request.form.get("translate-to")
+    translated = translated_text(
+        text_to_translate, translate_from, translate_to
+    )
+    return render_template(
+        "index.html",
+        languages=languages,
+        text_to_translate=translated,
+        translate_from=translate_to,
+        translate_to=translate_from,
+        translated=text_to_translate,
+    )
+
+
 def translated_text(text_to_translate, translate_from, translate_to):
     return GoogleTranslator(
         source=translate_from, target=translate_to
