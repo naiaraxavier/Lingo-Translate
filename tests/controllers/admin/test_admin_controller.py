29b1b8fbdd118805e5ac45e15607dc525f61865f
Naiara Martins <naiaraxf@gmail.com>
2023-12-12 16:21:15 -0300

    Task 10 completed :white_check_mark:

--- tests/controllers/admin/test_admin_controller.py
+++ tests/controllers/admin/test_admin_controller.py
@@ -1,36 +1,25 @@
-import pytest
 from src.models.user_model import UserModel
 from src.models.history_model import HistoryModel
-import json
 from bson import ObjectId
+import pytest
+import json
 
 
 @pytest.fixture
 def valid_user():
     user_data = {
         "_id": ObjectId("65738f2224c6e36e4ae23ae1"),
-        "name": "token",
-        "level": "token",
-        "token": "token",
+        "name": "Peter",
+        "level": "admin",
+        "token": "Token_123",
     }
     user = UserModel(user_data)
     user.save()
     return user
 
 
-def invalid_user():
-    user_data = {
-        "_id": ObjectId("token"),
-        "name": "token",
-        "level": "token",
-        "token": "token",
-    }
-    user = UserModel(user_data)
-    user.save()
-    return user
-
-
-def test_history_delete(app_test, valid_user):
+# Função auxiliar para criar um histórico válido para exclusão
+def create_valid_history_for_deletion():
     HistoryModel(
         {
             "_id": ObjectId("6577888e26d885c273b3c56f"),
@@ -40,9 +29,15 @@ def test_history_delete(app_test, valid_user):
         }
     ).save()
 
+
+# Função auxiliar para verificar o número inicial de históricos
+def assert_initial_history_count():
     history_before_delete = HistoryModel.list_as_json()
     assert len(json.loads(history_before_delete)) == 1
 
+
+# Função para testar a exclusão de um histórico válido
+def assert_valid_history_deletion(app_test, valid_user):
     response = app_test.delete(
         "/admin/history/6577888e26d885c273b3c56f",
         headers={
@@ -50,12 +45,15 @@ def test_history_delete(app_test, valid_user):
             "User": valid_user.data["name"],
         },
     )
-
     assert response.status_code == 204
 
     history_after_delete = HistoryModel.list_as_json()
     assert len(json.loads(history_after_delete)) == 0
 
+
+# Função para testar exclusões inválidas (por exemplo, sem autorização)
+def assert_unauthorized_history_deletion(app_test, valid_user):
+    # Tentativa de exclusão sem autorização
     response = app_test.delete(
         "/admin/history/6577888e26d885c273b3c56f",
         headers={
@@ -63,9 +61,29 @@ def test_history_delete(app_test, valid_user):
             "User": "test",
         },
     )
-
     assert response.status_code == 401
 
+    # Tentativa de exclusão com nome e token trocados
+    response = app_test.delete(
+        "/admin/history/6577888e26d885c273b3c56f",
+        headers={
+            "Authorization": valid_user.data["name"],
+            "User": valid_user.data["token"],
+        },
+    )
+    assert response.status_code == 401
+
+    # Tentativa de exclusão com token e nome de usuário iguais
+    response = app_test.delete(
+        "/admin/history/6577888e26d885c273b3c56f",
+        headers={
+            "Authorization": "token",
+            "User": "token",
+        },
+    )
+    assert response.status_code == 401
+
+    # Tentativa de exclusão com ID inválido
     response = app_test.delete(
         "/admin/history/6577888e26d885c273b3c56fdsgdçlhjsdçh",
         headers={
@@ -73,9 +91,9 @@ def test_history_delete(app_test, valid_user):
             "User": valid_user.data["name"],
         },
     )
-
     assert response.status_code == 500
 
+    # Tentativa de exclusão em um endpoint inválido
     response = app_test.delete(
         "/admin/history/",
         headers={
@@ -83,9 +101,9 @@ def test_history_delete(app_test, valid_user):
             "User": valid_user.data["name"],
         },
     )
-
     assert response.status_code == 404
 
+    # Tentativa de exclusão sem token de autorização
     response = app_test.delete(
         "/admin/history/6577888e26d885c273b3c56f",
         headers={
@@ -93,9 +111,9 @@ def test_history_delete(app_test, valid_user):
             "User": valid_user.data["name"],
         },
     )
-
     assert response.status_code == 401
 
+    # Tentativa de exclusão sem nome de usuário
     response = app_test.delete(
         "/admin/history/6577888e26d885c273b3c56f",
         headers={
@@ -103,15 +121,21 @@ def test_history_delete(app_test, valid_user):
             "User": "",
         },
     )
-
     assert response.status_code == 401
 
+    # Tentativa de exclusão com usuários inválidos
     response = app_test.delete(
         "/admin/history/6577888e26d885c273b3c56f",
         headers={
-            "Authorization": invalid_user,
-            "User": invalid_user,
+            "Authorization": "invalid_user",
+            "User": "invalid_user",
         },
     )
-
     assert response.status_code == 401
+
+
+def test_history_delete(app_test, valid_user):
+    create_valid_history_for_deletion()
+    assert_initial_history_count()
+    assert_valid_history_deletion(app_test, valid_user)
+    assert_unauthorized_history_deletion(app_test, valid_user)
